8ae1ff6d3531c5e67a9b9ac70731922a3ecaf7c3
JakobFinstad <jakobf44@gmail.com>
2023-02-02 13:26:25 +0100

    Adding content of the multithreader and AsyncSearchSimulator

--- src/main/java/no/ntnu/idata2305/group14/computation/AsyncSearchSimulator.java
+++ src/main/java/no/ntnu/idata2305/group14/computation/AsyncSearchSimulator.java
@@ -1,4 +1,18 @@
 package no.ntnu.idata2305.group14.computation;
 
-public class AsyncSearchSimulator {
+import java.net.Socket;
+
+public class AsyncSearchSimulator implements Runnable {
+
+    protected Socket clientSocket = null;
+    protected String serverText = null;
+
+    public AsyncSearchSimulator(Socket clientSocket, String serverText) {
+        this.clientSocket = clientSocket;
+        this.serverText = serverText;
+    }
+
+    public void run() {
+
+    }
 }
--- src/main/java/no/ntnu/idata2305/group14/servers/MultiThreaderServer.java
+++ src/main/java/no/ntnu/idata2305/group14/servers/MultiThreaderServer.java
@@ -1,4 +1,46 @@
 package no.ntnu.idata2305.group14.servers;
 
-public class MultiThreaderServer {
+import no.ntnu.idata2305.group14.computation.AsyncSearchSimulator;
+
+import java.net.ServerSocket;
+
+public class MultiThreadedServer implements Runnable {
+
+    protected int serverPort = 8080;
+    protected ServerSocket serverSocket = null;
+    protected boolean isStopped = false;
+
+    public SingleThreadedServer(int port) {
+        this.serverPort = port;
+    }
+
+    public void run() {
+        openServerSocket();
+
+        while (!isStopped()) {
+            // wait for a connection
+
+            // on receiving a request, execute the heavy computation in a new thread
+            new Thread(
+                    new AsyncSearchSimulator(
+                            clientSocket,
+                            "Multithreaded Server"
+                    )
+            ).start();
+        }
+
+        System.out.println("Server Stopped.");
+    }
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop() {
+        // implementation to stop the server from the main thread if needed
+    }
+
+    private void openServerSocket() {
+        // open server socket here
+    }
 }
