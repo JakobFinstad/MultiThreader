bb6a97a3664f6ac1f2af85d222a6f0d13367fd1a
Ertshus <siverer@stud.ntnu.no>
2023-02-02 13:27:40 +0100

    Almost finished with SingleThreaderServer.

--- src/main/java/no/ntnu/idata2305/group14/servers/SingleThreaderServer.java
+++ src/main/java/no/ntnu/idata2305/group14/servers/SingleThreaderServer.java
@@ -1,4 +1,36 @@
 package no.ntnu.idata2305.group14.servers;
 
+import java.net.ServerSocket;
+
 public class SingleThreaderServer {
+    protected int serverPort = 8080;
+    protected ServerSocket serverSocket = null;
+    protected boolean isStopped = false;
+
+    public SingleThreaderServer(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public void run() {
+        openServerSocket();
+
+        while (!isStopped()) {
+            // wait for a connection
+            // on receiving a request, execute the heavy computation
+        }
+
+        System.out.println("Server Stopped.");
+    }
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop() {
+        // implementation to stop the server from the main thread if needed
+    }
+
+    private void openServerSocket() {
+        // open server socket here
+    }
 }
