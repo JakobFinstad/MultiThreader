a3a81da93d26dcf964ce528f2f9dbdafc3d534e0
Ertshus <siverer@stud.ntnu.no>
2023-02-02 13:32:12 +0100

    Almost finished with SearchingSimulator.

--- src/main/java/no/ntnu/idata2305/group14/computation/SearchingSimulator.java
+++ src/main/java/no/ntnu/idata2305/group14/computation/SearchingSimulator.java
@@ -1,4 +1,15 @@
 package no.ntnu.idata2305.group14.computation;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class SearchingSimulator {
+    public static void processClientRequest() throws Exception {
+        long time1 = System.currentTimeMillis();
+        System.out.println("Request processing started at: " + time1);
+        Thread.sleep(10 * 1000);
+        long time2 = System.currentTimeMillis();
+        System.out.println("Request processing ended at: " + time2);
+    }
 }
