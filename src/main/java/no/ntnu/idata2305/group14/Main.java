eccbc9010e26241898ba523442f4ce1d7460ba8c
Ertshus <siverer@stud.ntnu.no>
2023-02-09 10:59:38 +0100

    Finished main.

--- src/main/java/no/ntnu/idata2305/group14/Main.java
+++ src/main/java/no/ntnu/idata2305/group14/Main.java
@@ -1,11 +1,21 @@
 package no.ntnu.idata2305.group14;
 
 import no.ntnu.idata2305.group14.computation.SearchingSimulator;
+import no.ntnu.idata2305.group14.servers.MultiThreadedServer;
+import no.ntnu.idata2305.group14.servers.SingleThreaderServer;
 
 public class Main {
+
+    private static boolean singleThreadMode = true;
+
+    private static int serverPort = 8080;
+
     public static void main(String[] args) {
         System.out.println("Starting server");
-        Thread thread = new Thread();
-        thread.start();
+        if (singleThreadMode) {
+            new SingleThreaderServer(serverPort).run();
+        } else {
+            new MultiThreadedServer(serverPort).run();
+        }
     }
 }
